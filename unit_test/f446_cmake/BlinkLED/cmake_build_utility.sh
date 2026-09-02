4db8353c16b26038d04c62a16369aa810c9a78f5
TM-khoa <minhkhoa9a31314@gmail.com>
2026-09-02 10:43:13 +0700

    feat(F446,cmake): Add BlinkLED F446 CMake build

---
+++ unit_test/f446_cmake/BlinkLED/cmake_build_utility.sh
@@ -0,0 +1,11 @@
+#! /bin/zsh
+
+cmake \
+    -S . \
+    -B build \
+    -DCMAKE_TOOLCHAIN_FILE=./cmake/gcc-arm-none-eabi.cmake \
+    -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+    -G "Ninja" \
+    -DCMAKE_BUILD_TYPE=Debug 
+
+cmake --build build
